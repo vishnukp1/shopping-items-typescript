00c2a97594960c1e3a248075d7044b5d09f84e80
wishtony <vishnutoanother@gmail.com>
2024-02-03 15:13:39 +0530

    commit

--- src/components/SortedItem.tsx
+++ src/components/SortedItem.tsx
@@ -1,4 +1,5 @@
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
+import formatCurrency from "../utilities/formatCurrency"
 
 type SortedItemProps = {
 id : number
@@ -8,15 +9,23 @@ imgUrl:string
 }
 
 function SortedItem({id ,name, price ,imgUrl}:SortedItemProps ){
-  return (
- <Card>
+ const quantity = 0
+   return (
+ <Card className="h-100">
+   
     <Card.Img variant="top" src={imgUrl} height="200px" style={{objectFit:"cover"}}/>
- <Card.Body className="d-flex  justify-content-between align-item-baseline mb-4">
+    <Card.Body className="d-flex flex-column" >
+ <Card.Title className="d-flex  justify-content-between align-item-baseline mb-4">
     <span className="fs-2">{name}</span>
-    <span className="ms-2 text-muted">{price}</span>
+    <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+   </Card.Title>
+    <div className="mt-auto">
+      {quantity===0 ? (
+         < Button className="w-100">Add To Cart</Button>
+      ):null}
+    </div>
  </Card.Body>
  </Card>
-
   )
 }
 
