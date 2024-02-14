accec56ac04f8e1f12d4123b27dcaab984f991f9
wishtony <vishnutoanother@gmail.com>
2024-02-14 12:06:31 +0530

    feat: Add two buttons for add to cart

--- src/components/Navbarr.tsx
+++ src/components/Navbarr.tsx
@@ -1,41 +1,52 @@
-
-import { Button, Container, Nav, Navbar } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 function Navbarr() {
   return (
-    <Navbar sticky='top' className='bg-white shadow-sm mb-3'>
-        <Container >
-            <Nav>
-                <Nav.Link to="/" as={NavLink}>
-                    Home
-                </Nav.Link>
-                <Nav.Link to="/store" as={NavLink}>
-                   Store
-                </Nav.Link>
-                <Nav.Link to="/about" as={NavLink}>
-                    About
-                </Nav.Link>
-            </Nav>
-            <Button style={{width:"3rem",height:"3rem",position:"relative"}}
-            variant='outline-primary'
-            className='rounded-circle'
-            >
- <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="16.5" cy="18.5" r="1.5"/>
-  <circle cx="9.5" cy="18.5" r="1.5"/>
-  <path d="M18 16H8a1 1 0 0 1-.958-.713L4.256 6H3a1 1 0 0 1 0-2h2a1 1 0 0 1 .958.713L6.344 6H21a1 1 0 0 1 .937 1.352l-3 8A1 1 0 0 1 18 16zm-9.256-2h8.563l2.25-6H6.944z"/>
-  </svg>
-  <div className='rounded-circle bg-danger d-flex justify-content-center align-items-center' style={{color:"white",width:"1.5rem",height:"1.5rem",position:"absolute",buttom:0,
-right:0,transform:"transalate(25%,25%)"}}>
-
-  </div>
-            </Button>
-           
-
-        </Container>
+    <Navbar sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav>
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
+          variant="outline-primary"
+          className="rounded-circle"
+        >
+          <svg
+            width="24px"
+            height="24px"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="16.5" cy="18.5" r="1.5" />
+            <circle cx="9.5" cy="18.5" r="1.5" />
+            <path d="M18 16H8a1 1 0 0 1-.958-.713L4.256 6H3a1 1 0 0 1 0-2h2a1 1 0 0 1 .958.713L6.344 6H21a1 1 0 0 1 .937 1.352l-3 8A1 1 0 0 1 18 16zm-9.256-2h8.563l2.25-6H6.944z" />
+          </svg>
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              buttom: 0,
+              right: 0,
+              transform: "transalate(25%,25%)",
+            }}
+          ></div>
+        </Button>
+      </Container>
     </Navbar>
-  )
+  );
 }
 
-export default Navbarr
+export default Navbarr;
--- src/components/SortedItem.tsx
+++ src/components/SortedItem.tsx
@@ -1,32 +1,41 @@
-import { Button, Card } from "react-bootstrap"
-import formatCurrency from "../utilities/formatCurrency"
+import { Button, Card } from "react-bootstrap";
+import formatCurrency from "../utilities/formatCurrency";
 
 type SortedItemProps = {
-id : number
-name:string
-price:number
-imgUrl:string
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
+
+function SortedItem({ id, name, price, imgUrl }: SortedItemProps) {
+  const quantity = 0;
+  return (
+    <Card className="h-100">
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200px"
+        style={{ objectFit: "cover" }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex  justify-content-between align-item-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100">Add To Cart</Button>
+          ) : <div className="d-flex align-item-center flex-column" style={{gap:".5rem"}}>
+            <div className="d-flex align-item-center justify-content-center" style={{gap:".5rem"}}>
+               <Button>-</Button>
+               <span>{quantity}</span>
+            </div>
+            </div>}
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
 
-function SortedItem({id ,name, price ,imgUrl}:SortedItemProps ){
- const quantity = 0
-   return (
- <Card className="h-100">
-   
-    <Card.Img variant="top" src={imgUrl} height="200px" style={{objectFit:"cover"}}/>
-    <Card.Body className="d-flex flex-column" >
- <Card.Title className="d-flex  justify-content-between align-item-baseline mb-4">
-    <span className="fs-2">{name}</span>
-    <span className="ms-2 text-muted">{formatCurrency(price)}</span>
-   </Card.Title>
-    <div className="mt-auto">
-      {quantity===0 ? (
-         < Button className="w-100">Add To Cart</Button>
-      ):null}
-    </div>
- </Card.Body>
- </Card>
-  )
-}
-
-export default SortedItem
+export default SortedItem;
